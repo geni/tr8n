427f32326623b5ff3e32fb518ab912251bb188a2
Scott Steadman <scott.steadman@geni.com>
2021-04-13 14:13:15 -0700

    fix LanguageCaseRule.evaluate and apply
      Geni sometimes wraps token values in parenthesis which
      made it impossible for .evaluate to detect whether a
      token began or ended with a string.  I modified .evaluate
      to ignore non-word characters preceding or following the
      sought-after string.
    
      I modified .apply to pre/ap-pend to the matched part instead
      of the entire string.
    
      Issue: geni/geni#2581

--- app/models/tr8n/language_case_rule.rb
+++ app/models/tr8n/language_case_rule.rb
@@ -24,24 +24,24 @@
 class Tr8n::LanguageCaseRule < ActiveRecord::Base
   set_table_name :tr8n_language_case_rules
 
-  belongs_to :language_case,  :class_name => "Tr8n::LanguageCase"   
-  belongs_to :language,       :class_name => "Tr8n::Language"   
-  belongs_to :translator,     :class_name => "Tr8n::Translator"   
-  
+  belongs_to :language_case,  :class_name => "Tr8n::LanguageCase"
+  belongs_to :language,       :class_name => "Tr8n::Language"
+  belongs_to :translator,     :class_name => "Tr8n::Translator"
+
   serialize :definition
-  
+
   def self.by_id(id)
-    Tr8n::Cache.fetch("language_case_rule_#{id}") do 
+    Tr8n::Cache.fetch("language_case_rule_#{id}") do
       find_by_id(id)
     end
   end
-  
+
   def self.gender_options
     [["not applicable", "none"], ["unknown", "unknown"], ["male", "male"], ["female", "female"]]
   end
-  
+
   def self.condition_options(with_if = false)
-    opts = [["starts with", "starts_with"], ["does not start with", "does_not_start_with"], 
+    opts = [["starts with", "starts_with"], ["does not start with", "does_not_start_with"],
      ["ends in", "ends_in"], ["does not end in", "does_not_end_in"],
      ["is", "is"], ["is not", "is_not"]]
     return opts unless with_if
@@ -65,56 +65,56 @@ class Tr8n::LanguageCaseRule < ActiveRecord::Base
       return false if definition["gender"] == "male"    and object_gender != Tr8n::GenderRule.gender_object_value_for("male")
       return false if definition["gender"] == "female"  and object_gender != Tr8n::GenderRule.gender_object_value_for("female")
       return false if definition["gender"] == "unknown" and object_gender != Tr8n::GenderRule.gender_object_value_for("unknown")
-    end    
-  
+    end
+
     result1 = evaluate_part(value, 1)
     if definition["multipart"] == "true"
       result2 = evaluate_part(value, 2)
       return false if definition["operator"] == "and" and !(result1 and result2)
       return false if definition["operator"] == "or"  and !(result1 or result2)
-    end  
-    
+    end
+
     return result1
   end
-  
+
   def evaluate_part(token_value, index)
     values = sanitize_values(definition["value#{index}"])
-    
+
     case definition["part#{index}"]
-      when "starts_with" 
+      when "starts_with"
         values.each do |value|
-          return true if token_value.to_s =~ /^#{value.to_s}/  
+          return true if token_value.to_s =~ /^[\P{word}]*#{value.to_s}/
         end
         return false
-      when "does_not_start_with"         
+      when "does_not_start_with"
         values.each do |value|
-          return false if token_value.to_s =~ /^#{value.to_s}/  
+          return false if token_value.to_s =~ /^[\P{word}]*#{value.to_s}/
         end
         return true
       when "ends_in"
         values.each do |value|
-          return true if token_value.to_s =~ /#{value.to_s}$/  
+          return true if token_value.to_s =~ /#{value.to_s}[\P{word}]*$/
         end
         return false
-      when "does_not_end_in"         
+      when "does_not_end_in"
         values.each do |value|
-          return false if token_value.to_s =~ /#{value.to_s}$/  
+          return false if token_value.to_s =~ /#{value.to_s}[\P{word}]*$/
         end
         return true
-      when "is"         
+      when "is"
         return values.include?(token_value)
-      when "is_not"        
+      when "is_not"
         return !values.include?(token_value)
     end
-    
+
     false
   end
-  
+
   def apply(value)
     values = sanitize_values(definition["value1"])
     regex = values.join('|')
     case definition["operation"]
-      when "replace" 
+      when "replace"
         if definition["part1"] == "starts_with"
           return value.gsub(/\b(#{regex})/, definition["operation_value"])
         elsif definition["part1"] == "is"
@@ -122,65 +122,65 @@ class Tr8n::LanguageCaseRule < ActiveRecord::Base
         elsif definition["part1"] == "ends_in"
           return value.gsub(/(#{regex})\b/, definition["operation_value"])
         end
-      when "prepand" 
-        return "#{definition["operation_value"]}#{value}"
-      when "append"        
-        return "#{value}#{definition["operation_value"]}"
+      when "prepand"
+        return value.gsub(/\b(#{regex})/, "#{definition["operation_value"]}\\1")
+      when "append"
+        return value.gsub(/(#{regex})\b/, "\\1#{definition["operation_value"]}")
     end
-    
+
     value
   end
-  
+
   def sanitize_values(values)
     return [] unless values
-    values.split(",").collect{|val| val.strip} 
+    values.split(",").collect{|val| val.strip}
   end
-  
+
   def humanize_values(values)
     sanitize_values(values).join(", ")
   end
-  
+
   def description
     return "undefined rule" if definition.blank?
-    
+
     desc = "If"
     if definition["gender"] != "none"
       desc << " subject"
       if ["male", "female"].include?(definition["gender"])
         desc << " is a <strong>#{definition["gender"]}</strong>"
-      else  
+      else
         desc << " <strong>has an unknown gender</strong>"
       end
     end
     desc << " and" unless desc == "If"
     desc << " token value"
     desc << describe_part(1)
-  
+
     if definition["multipart"] == "true"
       desc << " " << definition["operator"]
       desc << describe_part(2)
     end
-    
+
     desc << ", then"
     case definition["operation"]
       when "replace" then desc << " replace it with"
-      when "prepand" then desc << " prepand the value with"        
-      when "append" then desc << " append the value with"        
+      when "prepand" then desc << " prepand the value with"
+      when "append" then desc << " append the value with"
     end
     desc << " <strong>'" << humanize_values(definition["operation_value"]) << "'</strong> "
   end
-  
+
   def describe_part(index)
     desc = ""
     case definition["part#{index}"]
       when "starts_with" then desc << " starts with"
-      when "does_not_start_with" then desc << " does not start with"        
-      when "ends_in" then desc << " ends in"        
-      when "does_not_end_in" then desc << " does not end in"        
-      when "is" then desc << " is"        
-      when "is_not" then desc << " is not"        
+      when "does_not_start_with" then desc << " does not start with"
+      when "ends_in" then desc << " ends in"
+      when "does_not_end_in" then desc << " does not end in"
+      when "is" then desc << " is"
+      when "is_not" then desc << " is not"
     end
     desc << " <strong>'" << humanize_values(definition["value#{index}"]) << "'</strong>"
   end
-  
+
 end
