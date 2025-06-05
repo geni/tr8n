568790c48b99e2ecac31c08030c72128aa759ed1
Scott Steadman <scott.steadman@geni.com>
2025-06-05 10:43:56 -0700

    fix sum queries

--- app/models/tr8n/monthly_language_metric.rb
+++ app/models/tr8n/monthly_language_metric.rb
@@ -47,9 +47,12 @@ class Tr8n::MonthlyLanguageMetric < Tr8n::LanguageMetric
 
   def update_metrics!
     attribs = default_attributes
+
     attribs.each do |key, value|
-      attribs[key] = Tr8n::DailyLanguageMetric.sum(key, :conditions => ["language_id = ? and metric_date >= ? and metric_date < ?", language_id, metric_date, metric_date + 1.month])
+      attribs[key] = Tr8n::DailyLanguageMetric.where(['language_id = ? and metric_date >= ? and metric_date < ?', language_id, metric_date, metric_date + 1.month])
+                                              .sum(key)
     end
+
     update(attribs)
   end
 
