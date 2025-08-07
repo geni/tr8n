31957b10ee6e395be5d1c4a2f0368e523c1a1d3a
Scott Steadman <scott.steadman@geni.com>
2025-08-07 11:33:08 -0700

    change mount point to /foo
      I want to make sure all the links work.
      We have a lot of hard-coded /tr8n/ references.

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  mount Tr8n::Engine => "/tr8n"
+  # mount at /foo to make sure the links work
+  mount Tr8n::Engine => '/foo'
 
   get '/dummy/switch_user', to: 'dummy#switch_user', as: :switch_user
 
