415ac22b5aab2aa883bd7f4188519b77e3a120a0
Scott Steadman <scott.steadman@geni.com>
2025-08-13 10:14:43 -0700

    add translator to db/seeds.rb

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -4,6 +4,10 @@ Tr8n::Language.create!(:locale => 'en-US', :english_name => 'English', :native_n
 Tr8n::Language.create!(:locale => 'en-PL', :english_name => 'Pig Latin', :native_name => 'Igpay Atinlay')
 
 User.destroy_all
-User.create!(:id => 0, :name => 'Guest', :guest => true)
-User.create!(:id => 1, :name => 'User')
-User.create!(:id => 2, :name => 'Admin', :admin => true)
+guest = User.create!(:id => 0, :name => 'Guest', :guest => true)
+admin = User.create!(:id => 1, :name => 'Admin', :admin => true)
+user  = User.create!(:id => 2, :name => 'User')
+trans = User.create!(:id => 3, :name => 'Translator')
+
+Tr8n::Translator.destroy_all
+Tr8n::Translator.create!(:name => 'Default Translator', :user => trans)
