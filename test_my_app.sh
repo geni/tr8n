95bde55029fcbbcaa0b027e5b2776a69847570f2
Scott Steadman <scott.steadman@geni.com>
2026-04-09 09:21:07 -0700

    update test_my_app.sh
      I'm not sure why I was deleting that stuff.

--- test_my_app.sh
+++ test_my_app.sh
@@ -4,9 +4,6 @@ bundle config --local clean true
 bundle config --local path vendor/bundle
 bundle config --local without vscode
 
-rm -rf Gemfile.lock vendor/bundle
 bundle install
-
 bundle exec rails db:create db:migrate
-
 bundle exec rails test
