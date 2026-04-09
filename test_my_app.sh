5abca11c40cc401296ac1a24470a8ba7c78d82f6
Scott Steadman <scott.steadman@geni.com>
2026-04-09 07:49:52 -0700

    update test_my_app.sh
      Jenkins handles the stuff I removed.

--- test_my_app.sh
+++ test_my_app.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-unset RBENV_VERSION
-export RBENV_ROOT=/opt/rbenv
-export PATH=${RBENV_ROOT}/shims:${PATH}:${RBENV_ROOT}/bin
-
-git gc
-
-bundle config --local build.pg "--with-pg-config=/usr/pgsql-15/bin/pg_config"
 bundle config --local clean true
 bundle config --local path vendor/bundle
 bundle config --local without vscode
