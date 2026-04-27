c775db3d6c5b0d6e72ba593f90388a14b8fd0ce6
Mike Stangel <mike@geni.com>
2026-04-27 14:35:33 -0700

    fix tr8n config relative path

--- lib/tr8n/config.rb
+++ lib/tr8n/config.rb
@@ -263,7 +263,10 @@ class Tr8n::Config
   end
 
   def self.load_yml(file_path, for_env = env)
-    yml = YAML.load_file("#{root}#{file_path}")
+    file = "#{root}/#{file_path}"
+    file = "#{Tr8n::Engine.root}/#{file_path}" unless File.exist?(file)
+
+    yml = YAML.load_file(file) # Rails 3.1: , :aliases => true)
     yml = yml[for_env] unless for_env.nil?
     HashWithIndifferentAccess.new(yml)
   end
@@ -273,11 +276,12 @@ class Tr8n::Config
   end
 
   def self.config
-    @config ||= load_yml("/config/tr8n/config.yml")
+    @config ||= load_yml("config/tr8n/config.yml")
   end
 
+
   def self.default_languages
-    @default_languages ||= load_yml("/config/tr8n/site/default_languages.yml", nil)
+    @default_languages ||= load_yml("config/tr8n/site/default_languages.yml", nil)
   end
 
   def self.format
@@ -289,21 +293,21 @@ class Tr8n::Config
   end
 
   def self.default_decoration_tokens
-    @default_decoration_tokens ||= load_yml("/config/tr8n/tokens/decorations.yml", nil)
+    @default_decoration_tokens ||= load_yml("config/tr8n/tokens/decorations.yml", nil)
     @default_decoration_tokens[format]
   end
 
   def self.default_data_tokens
-    @default_data_tokens ||= load_yml("/config/tr8n/tokens/data.yml", nil)
+    @default_data_tokens ||= load_yml("config/tr8n/tokens/data.yml", nil)
     @default_data_tokens[format]
   end
 
   def self.default_glossary
-    @default_glossary ||= load_yml("/config/tr8n/site/default_glossary.yml", nil)
+    @default_glossary ||= load_yml("config/tr8n/site/default_glossary.yml", nil)
   end
 
   def self.features
-    @features ||= load_yml("/config/tr8n/site/features.yml")
+    @features ||= load_yml("config/tr8n/site/features.yml")
   end
 
   def self.enabled?
@@ -323,7 +327,7 @@ class Tr8n::Config
   end
 
   def self.default_shortcuts
-    @default_shortcuts ||= load_yml("/config/tr8n/site/shortcuts.yml", nil)
+    @default_shortcuts ||= load_yml("config/tr8n/site/shortcuts.yml", nil)
   end
 
   def self.enable_inline_translations?
@@ -761,7 +765,7 @@ class Tr8n::Config
   # get rules for specified locale, or get default language rules
   def self.load_default_rules(rules_type, locale = default_locale)
     @default_rules ||= {}
-    @default_rules[rules_type] ||= load_yml("/config/tr8n/rules/default_#{rules_type}_rules.yml", nil)
+    @default_rules[rules_type] ||= load_yml("config/tr8n/rules/default_#{rules_type}_rules.yml", nil)
     rules_for_locale = @default_rules[rules_type][locale.to_s]
 
     return rules_for_locale.values unless rules_for_locale.nil?
@@ -794,7 +798,7 @@ class Tr8n::Config
   end
 
   def self.default_language_cases
-    @default_language_cases ||= load_yml("/config/tr8n/rules/default_language_cases.yml", nil)
+    @default_language_cases ||= load_yml("config/tr8n/rules/default_language_cases.yml", nil)
   end
 
   #########################################################
@@ -1015,7 +1019,7 @@ class Tr8n::Config
   end
 
   def self.default_relationship_keys
-    @default_relationship_keys ||= load_yml("/config/tr8n/data/default_relationship_keys.yml", nil)
+    @default_relationship_keys ||= load_yml("config/tr8n/data/default_relationship_keys.yml", nil)
   end
 
   def self.init_configuration_keys
@@ -1041,7 +1045,7 @@ class Tr8n::Config
   end
 
   def self.default_configuration_keys
-    @default_configuration_keys ||= load_yml("/config/tr8n/data/default_configuration_keys.yml", nil)
+    @default_configuration_keys ||= load_yml("config/tr8n/data/default_configuration_keys.yml", nil)
   end
 
   def self.guid
