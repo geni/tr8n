1c7f09e2567ade60f4e2db77d6809cfa11721029
Scott Steadman <scott.steadman@geni.com>
2026-05-06 11:36:59 -0700

    remove Tr8n::Extender
      Replaced by ActiveSupport::Concern

--- app/models/tr8n/application_record.rb
+++ app/models/tr8n/application_record.rb
@@ -2,7 +2,7 @@ module Tr8n
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
 
-    extend Tr8n::ActiveDumper
+    include Tr8n::ActiveDumper
 
     self.table_name_prefix = 'tr8n_'
   end
--- lib/tr8n/active_dumper.rb
+++ lib/tr8n/active_dumper.rb
@@ -1,27 +1,26 @@
 
-require File.expand_path(File.dirname(__FILE__) + "/extender") 
+module Tr8n
+  class DumperException < StandardError; end
 
-class Tr8n::DumperException < StandardError; end
-module Tr8n::ActiveDumper
-  extend Tr8n::Extender
+  module ActiveDumper
+    extend ActiveSupport::Concern
 
-  module InstanceMethods
     def _dump(ignored)
       data = {:attributes => @attributes, :new_record => @new_record}
       Marshal.dump(data)
     end
-  end
-  
-  module ClassMethods
-    def _load(str)
-      data = Marshal.load(str)
-      
-      raise Tr8n::DumperException, 'invalid format' if not data.kind_of?(Hash) or data.keys.to_set != [:attributes, :new_record].to_set
 
-      instance = new
-      instance.instance_variable_set(:@attributes, data[:attributes])
-      instance.instance_variable_set(:@new_record, data[:new_record])
-      instance
+    module ClassMethods
+      def _load(str)
+        data = Marshal.load(str)
+
+        raise Tr8n::DumperException, 'invalid format' if not data.kind_of?(Hash) or data.keys.to_set != [:attributes, :new_record].to_set
+
+        instance = new
+        instance.instance_variable_set(:@attributes, data[:attributes])
+        instance.instance_variable_set(:@new_record, data[:new_record])
+        instance
+      end
     end
-  end
-end
+  end # module ActiveDumper
+end # module Tr8n
