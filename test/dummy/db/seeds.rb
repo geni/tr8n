4802f892622516a0aeb6e8d7bb4f3995c659bdfd
Scott Steadman <scott.steadman@geni.com>
2026-05-06 09:11:40 -0700

    update db/seeds.rb
      I want some feedback that this is working.

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -1,6 +1,7 @@
 #
 # Platform stuff
 #
+puts "Creating Users..."
 User.destroy_all
 guest = User.create!(:id => 0, :name => 'Guest')
 admin = User.create!(:id => 1, :name => 'Admin')
@@ -10,10 +11,12 @@ dev   = User.create!(:id => 3, :name => 'Developer')
 #
 # Tr8n stuff
 #
+puts "Creating languages..."
 Tr8n::Language.destroy_all
 en_US = Tr8n::Language.create!(:locale => 'en-US', :english_name => 'English', :native_name => 'English', :enabled => true)
 en_PL = Tr8n::Language.create!(:locale => 'en-PL', :english_name => 'Pig Latin', :native_name => 'Igpay Atinlay', :enabled => true, :fallback_language_id =>en_US.id)
 
+puts "Creating translators..."
 Tr8n::Translator.destroy_all
 trans = User.create!(:id => 4, :name => 'Translator')
 Tr8n::Translator.create!(:name => 'Default Translator', :user_id => trans.id)
