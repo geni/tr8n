748295b87abb8fa4ad9fa41f3a836830421bc62d
Mike Stangel <mike@geni.com>
2026-06-08 15:31:51 -0700

    must verify keys outside of the cache hit

--- app/models/tr8n/translation_key.rb
+++ app/models/tr8n/translation_key.rb
@@ -74,11 +74,11 @@ class Tr8n::TranslationKey < ApplicationRecord
 
       mark_as_admin(existing_key, options)
       update_default_locale(existing_key, options)
-      verify_key(existing_key, options)
 
       existing_key
     end
 
+    verify_key(existing_key, options)
     track_source(tkey, options)
     tkey
   end
